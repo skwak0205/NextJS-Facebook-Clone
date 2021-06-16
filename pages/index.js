30d9c9f407bdfc0f06c3c263369b5aedbf5b9511
Soojung Kwak <gsj2464@gmail.com>
2021-06-16 09:20:15 +0900

    next auth 추가

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from 'next-auth/client';
 import Head from 'next/head';
 import Header from '../components/Header';
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
+
   return (
     <div>
       <Head>
@@ -19,4 +22,14 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }

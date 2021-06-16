3fd66274ecec7c86292ec489389124a95d129c44
Soojung Kwak <gsj2464@gmail.com>
2021-06-16 21:07:25 +0900

    firebase db & storage에 연결 / 업로드 기능 구현

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,13 +1,76 @@
 import { useSession } from 'next-auth/client';
+import { useRef, useState } from 'react';
 import Image from 'next/image';
+import { db, storage } from '../firebase';
+import firebase from 'firebase';
 import { EmojiHappyIcon } from '@heroicons/react/outline';
 import { CameraIcon, VideoCameraIcon } from '@heroicons/react/solid';
 
 const InputBox = () => {
   const [session] = useSession();
+  const inputRef = useRef(null);
+  const filePickerRef = useRef(null);
+
+  const [imageToPost, setImageToPost] = useState(null);
 
   const sendPost = (e) => {
     e.preventDefault();
+
+    if (!inputRef.current.value) return;
+
+    db.collection('posts')
+      .add({
+        message: inputRef.current.value,
+        name: session.user.name,
+        email: session.user.email,
+        image: session.user.image,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then((doc) => {
+        if (imageToPost) {
+          const uploadTask = storage
+            .ref(`posts/${doc.id}`)
+            .putString(imageToPost, 'data_url');
+          removeImage();
+
+          uploadTask.on(
+            'state_change',
+            null,
+            (error) => console.error(error),
+            () => {
+              storage
+                .ref('posts')
+                .child(doc.id)
+                .getDownloadURL()
+                .then((url) => {
+                  db.collection('posts').doc(doc.id).set(
+                    {
+                      postImage: url,
+                    },
+                    { merge: true }
+                  );
+                });
+            }
+          );
+        }
+      });
+
+    inputRef.current.value = '';
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result);
+    };
+  };
+
+  const removeImage = () => {
+    setImageToPost(null);
   };
 
   return (
@@ -25,12 +88,23 @@ const InputBox = () => {
           <input
             className='rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none'
             type='text'
+            ref={inputRef}
             placeholder={`What's on your mind, ${session.user.name}?`}
           />
           <button type='submit' hidden onClick={sendPost}>
             Submit
           </button>
         </form>
+
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className='flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer'
+          >
+            <img className='h-10 object-contain' src={imageToPost} alt='' />
+            <p className='text-xs text-red-500 text-center'>Remove</p>
+          </div>
+        )}
       </div>
 
       <div className='flex justify-evenly p-3 border-t'>
@@ -39,9 +113,18 @@ const InputBox = () => {
           <p className='text-xs sm:text-sm xl:text-base'>Live Video</p>
         </div>
 
-        <div className='inputIcon'>
+        <div
+          onClick={() => filePickerRef.current.click()}
+          className='inputIcon'
+        >
           <CameraIcon className='h-7 text-green-400' />
           <p className='text-xs sm:text-sm xl:text-base'>Photo/Video</p>
+          <input
+            type='file'
+            hidden
+            ref={filePickerRef}
+            onChange={addImageToPost}
+          />
         </div>
 
         <div className='inputIcon'>

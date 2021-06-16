dfc55a32b2cbae3e662a53d73a74f102da9209b0
Soojung Kwak <gsj2464@gmail.com>
2021-06-16 10:08:28 +0900

    sidebar 추가

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,21 @@ import { getSession } from 'next-auth/client';
 import Login from '../components/Login';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 export default function Home({ session }) {
   if (!session) return <Login />;
 
   return (
-    <div>
+    <div className='h-screen bg-gray-100 overflow-hidden'>
       <Head>
         <title>Facebook Clone</title>
       </Head>
 
       <Header />
 
-      <main>
-        {/* Sidebar */}
+      <main className='flex'>
+        <Sidebar />
 
         {/* Feed */}
 

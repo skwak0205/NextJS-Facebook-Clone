cabcae415955fd42c998f27c1f855957b6bee30a
Soojung Kwak <gsj2464@gmail.com>
2021-06-17 10:18:14 +0900

    firebase config

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyD1gIJD30VLntcu5DndQnce9xh1IqNR6Sw',
+  authDomain: 'next-facebook-clone-196aa.firebaseapp.com',
+  projectId: 'next-facebook-clone-196aa',
+  storageBucket: 'next-facebook-clone-196aa.appspot.com',
+  messagingSenderId: '323982944204',
+  appId: '1:323982944204:web:8971962009e1f681255f69',
 };
 
 const app = !firebase.apps.length
